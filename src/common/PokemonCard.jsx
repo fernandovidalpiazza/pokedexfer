2910a05f739890a85b86496053588ffcc33f109f
fernandovidalpiazza <fervidalpiazza@gmail.com>
2023-11-03 09:21:58 -0300

    terminado 3

---
+++ src/common/PokemonCard.jsx
@@ -0,0 +1,79 @@
+import { setStatusBarBackgroundColor } from "expo-status-bar";
+import React from "react";
+import { Text, View, TouchableOpacity,Image, StyleSheet } from "react-native";
+
+export default function PokemonCard({ pokemon }) {
+  return (
+    <TouchableOpacity onPress={() => console.log("Seleccione un Pokemon")}>
+     <View style={{ ...styles.cardContainer, backgroundColor: pokemon.color }}>
+
+        <Text style = {styles.name}>
+          {pokemon.name} {pokemon.id}
+        </Text>
+        <View style={styles.pokeballContainer}> 
+          <Image source={require("../assets/picachu.png")} style={styles.picachu} /> 
+        </View>
+        <Image source={{uri: pokemon.picture}} style={styles.pokemonImagen} />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer:{
+    marginHorizontal: 10,
+    height:120,
+width:160,
+marginBottom:25,
+borderRadius:10,
+shadowColor:"#000",
+textShadowOffset:{
+  width:0,
+  height:2,
+
+},
+
+shadowOpacity:0.25,
+shadowRadius:4,
+elevation:5,
+ },
+ name:{
+  color: "witesmoke",
+  fontSize:20,
+  top:20,
+  left:10,
+   },
+   pokeballContainer:{
+    width:100,
+    height:100,
+    position: "absolute",
+    bottom:0,
+    overflow:"hidden",
+    opacity:0.4,
+
+   },
+   picachu:{
+    width:100,
+    height:100,
+    position:"absolute",
+    bottom:-25,
+    right:-25,
+
+   },
+   pokemonImagen:{
+      width:100,
+      height:100,
+      position:"absolute",
+      right:-15,
+      bottom:-10,
+
+   },
+
+})
+
+
+
+
+
+
+
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Image, Text, View, StyleSheet, FlatList, ActivityIndicator } from "react-native";
+import { usePokemonPaginate } from "../hooks/usePokemonPaginate";
+import PokemonCard from "../common/PokemonCard";
+
+export default function HomeScreen() {
+  const { pokemonList, loadPokemons } = usePokemonPaginate();
+
+  return (
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.bgPokeball}
+      />
+      {pokemonList.length > 0 && (
+        <View style={styles.containerList}>
+          <FlatList
+            data={pokemonList}
+            numColumns={2}
+            keyExtractor={(pokemon) => String(pokemon.id)}
+            renderItem={({ item }) => <PokemonCard pokemon={item} />}
+            ListHeaderComponent={<Text style={styles.title}>Pokedex</Text>}
+            showsVerticalScrollIndicator={false}
+            ListFooterComponent={
+              <ActivityIndicator style={{ height: 200 }} size={50} />
+            }
+            onEndReached={loadPokemons}
+            onEndReachedThreshold={0.3}
+          />
+        </View>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  bgPokeball: {
+    width: 300,
+    height: 300,
+    position: "absolute",
+    top: -130,
+    right: -130,
+    marginHorizontal: 20,
+    opacity: 0.2,
+  },
+  containerList: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginHorizontal: 20,
+  },
+});
